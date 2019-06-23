bee6185e3a074e79575e5b51fab13f01622eb608
Nabeel Chaudhry <nschaudh@gmail.com>
2019-06-23 18:57:14 -0400

    Added dropdown + level circles for select screen

--- app/src/main/java/ca/uwaterloo/tonality/LevelSelectActivity.java
+++ app/src/main/java/ca/uwaterloo/tonality/LevelSelectActivity.java
@@ -2,13 +2,50 @@ package ca.uwaterloo.tonality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class LevelSelectActivity extends AppCompatActivity {
+public class LevelSelectActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+
+        Spinner spinner = findViewById(R.id.scaleSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.scales, R.layout.spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String scale = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), scale, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void onLevelClick(View view) {
+        switch (view.getId()) {
+            case R.id.levelNote1:
+                Intent intent = new Intent(LevelSelectActivity.this, MainGameActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
